3264ed7f55b59c9668bbf47c9513feb16bef81a0
Stefanos Kornilios Mitsis Poiitidis <skmp@nillware.com>
2013-11-08 15:59:16 +0200

    Initial commit.

---
+++ app.js
@@ -0,0 +1,17 @@
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
+
+console.log("server started at 8080");
